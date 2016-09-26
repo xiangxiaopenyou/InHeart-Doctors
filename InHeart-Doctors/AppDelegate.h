977cf7a49b32f4d68f9bbe42f0fd11cf37216183
项小盆友 <1259653875@qq.com>
2016-09-26 09:26:58 +0800

    医生端

---
+++ InHeart-Doctors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InHeart-Doctors
+//
+//  Created by 项小盆友 on 16/9/26.
+//  Copyright © 2016年 项小盆友. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
